dc61c5c5bbac9daa6cc8c74d682b570ea8d7231d
Makoto Okabe <mako4th@snow.plala.or.jp>
2023-06-21 21:38:43 +0900

    update

--- ViolinTraner4/com_amakusaweb_LaunchScreenViewController.m
+++
@@ -1,9 +0,0 @@
-//
-//  com_amakusaweb_LaunchScreenViewController.m
-//  TuningTone
-//
-//  Created by MakotoOkabe on 2023/06/21.
-//  Copyright © 2023 MAKOTO OKABE. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
